a65fc6f46df69e304b63e3f639b25b84be2ad3ac
apostoli <79337131+apostolos-geyer@users.noreply.github.com>
2024-05-02 15:23:57 -0400

    update cli, update stages to handle multiple different file dates and process and log separately, lint

--- src/raipy_elt/__main__.py
+++ src/raipy_elt/__main__.py
@@ -1,26 +1,66 @@
+import os
+from pathlib import Path
+
+import click
 
 
 def raw_to_bronze():
     import os
     from pathlib import Path
 
-    if not (data_dir := os.getenv('DATA_DIR', None)):
-        print('DATA_DIR environment variable not set. Please set it to the path of the data directory'
-                         '\n(The directory that contains the subdirectory 0-raw, etc...)')
-        
-        print('i.e, call the script like this: DATA_DIR=/path/to/data raw2bronze')
+    if not (data_dir := os.getenv("DATA_DIR", None)):
+        print(
+            "DATA_DIR environment variable not set. Please set it to the path of the data directory"
+            "\n(The directory that contains the subdirectory 0-raw, etc...)"
+        )
+
+        print("i.e, call the script like this: DATA_DIR=/path/to/data raw2bronze")
         exit(1)
 
-    from raipy_elt.extract.raw_to_bronze import (
-        gen_stages, 
-        run_stages
-    )
-
-    from raipy_elt.extract.configs import (
-        TABLES, RAW
-    )
+    from raipy_elt.extract.configs import RAW, TABLES
+    from raipy_elt.extract.raw_to_bronze import gen_stages, run_stages
 
     for table in TABLES[RAW]:
         stages = gen_stages(table, Path(data_dir))
         run_stages(stages)
-    
+
+
+@click.group()
+def raipy_elt():
+    pass
+
+
+@raipy_elt.command()
+@click.option(
+    "--table",
+    "-t",
+    type=click.Choice(["questions", "scoring"]),
+    help="The table to extract",
+    required=True,
+)
+@click.option(
+    "--data-dir",
+    "-d",
+    type=str,
+    help="The directory containing the raw data",
+    default=lambda: os.environ.get("DATA_DIR", None),
+)
+def ingest_raw(table: str, data_dir: os.PathLike | None):
+    """
+    Ingest raw data into the bronze layer
+    """
+    from raipy_elt.extract.raw_to_bronze import gen_stages, run_stages
+
+    if data_dir is None:
+        data_dir = click.prompt(
+            "Please enter the path to the data directory.."
+            "\nhint: you can set the DATA_DIR environment variable or pass -d /path/to/data_dir to avoid the prompt."
+        )
+
+    stage_groups = gen_stages(table, Path(data_dir))
+    for stages in stage_groups:
+        run_stages(stages)
+
+
+def main():
+    raipy_elt()
